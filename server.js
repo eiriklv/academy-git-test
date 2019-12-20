32c1574510df0c8ad0b869b549cbff868a38a60e
Eirik Vullum <evullum@gmail.com>
2019-12-20 10:24:30 +0100

    Add some stuff to the server

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ console.log('this is the server');
 
 console.log('this is the server 123');
 
+
 console.log('this is the server 123');
 
 console.log('this is the server');
@@ -16,3 +17,9 @@ console.log('this is the server');
 console.log('this is the server');
 
 console.log('this is the server');
+
+const elem = document.getElementById('gfdgdfgdfg');
+
+elem.addEventListener('click', () => {
+  console.log('clicked');
+})
