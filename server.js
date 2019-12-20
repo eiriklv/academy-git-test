373bb350dd984da0fd2c1252785ea78f7d346e31
Eirik Vullum <evullum@gmail.com>
2019-12-20 10:13:49 +0100

    Add some things to the server to make it work

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
 console.log('this is the server');
+console.log('this is the server');
+
+console.log('this is the server');
+
+console.log('this is the server');
+
+console.log('this is the server 123');
+
+console.log('this is the server 123');
+
+console.log('this is the server');
+
+console.log('this is the server');
+
+console.log('this is the server');
+
+console.log('this is the server');
