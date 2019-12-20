be730c7ad4d37f2a0415d1e205b739478d403161
RobinRF <robinfiske87@gmail.com>
2019-12-20 10:19:48 +0100

    Husk å legge inn masse prompts i alt man lager.

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ console.log('this is the server');
 
 console.log('this is the server 123');
 
+console.log('Sjekk ut bloggen til Eirik :)');
+
+alert('Har noen vært i 4. etasje?');
+
 console.log('this is the server 123');
 
 console.log('this is the server');
